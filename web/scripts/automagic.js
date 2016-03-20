30df38110f6c89224aaf59cd0b3682f61b8fc696
Michael <Michael@Monster-M>
2016-03-20 15:37:54 +0100

    scripts flettet sammen i automagic, husk feltnavnet til søgning skal hedde "automagic"

--- web/scripts/automagic.js
+++ web/scripts/automagic.js
@@ -21,7 +21,71 @@ document.getElementById("automagic").addEventListener("keyup", function(){
     
 });
 
+var addCompany = function () {
+//    var formData = JSON.stringify($("#userForm").serializeArray());
+    var company = {
+        "companyName" : document.getElementById('companyName').value,
+        "cvr" : document.getElementById('cvr').value,
+        "description" : document.getElementById('description').value,
+        "numEmployees" : document.getElementById('numEmployees').value,
+        "marketValue" : document.getElementById('marketValue').value
+        };
+    
 
+    $.ajax({
+        type: "POST",
+        url: "api/search/",
+        data: JSON.stringify(company),
+        success: function () {},
+        dataType: "json",
+        contentType: "application/json"
+    });
+};
+
+var addPerson = function () {
+//    var formData = JSON.stringify($("#userForm").serializeArray());
+    var person2 = {
+        "firstName" : document.getElementById('firstName').value,
+        "lastName" : document.getElementById('lastName').value,
+        "email" : document.getElementById('email').value,
+        "phones" : [{
+            "number" : document.getElementById('phoneNumber').value,
+            "description" : document.getElementById('phoneDescription').value
+        }]
+        };
+    
+
+    $.ajax({
+        type: "POST",
+        url: "api/search/",
+        data: JSON.stringify(person2),
+        success: function () {},
+        dataType: "json",
+        contentType: "application/json"
+    });
+};
+
+var updatePerson = function () {
+//    var formData = JSON.stringify($("#userForm").serializeArray());
+    var person = {
+        "firstName" : document.getElementById('firstName').value,
+        "lastName" : document.getElementById('lastName').value,
+        "email" : document.getElementById('email').value,
+        "phones" : [{
+            "number" : document.getElementById('phoneNumber').value,
+            "description" : document.getElementById('phoneDescription').value
+        }]
+        };
+  
+    $.ajax({
+        type: "PUT",
+        url: "api/search/",
+        data: JSON.stringify(person),
+        success: function () {},
+        dataType: "json",
+        contentType: "application/json"
+    });
+};
 
 //document.addEventListener("click", function(){
 //    document.getElementById("automagic").value = "Hello World";
